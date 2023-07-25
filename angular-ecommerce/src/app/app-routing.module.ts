4891455b1b32c94a2d6175e66feee71a9b8d7743
vladimir8sabaev <vladimir8sabaev@mail.ru>
2023-07-25 12:07:12 +0300

    feat: add routing, navbar and basic components

--- angular-ecommerce/src/app/app-routing.module.ts
+++ angular-ecommerce/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'products',
+    component: HomeComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'sale',
+    component: SaleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
